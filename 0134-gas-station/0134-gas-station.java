c27cb4e2d48a4763986428e66a58d8b6c4c6e6d8
Devendra Sharma <146749164+Devendra2005Sharma@users.noreply.github.com>
2026-08-19 12:31:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -9,7 +9,7 @@ class Solution {
 
             tank += gas[i] - cost[i];
             if(tank<0){
-                start = i+1;
+                start = i+2;
                 tank = 0;
             }
         }
